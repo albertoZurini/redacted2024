f9918365baa4b2c2862c27308f732348f8fa4d4d
Tú <22521591@gm.uit.edu.vn>
2024-09-26 19:39:17 +0700

    [update] cải thiện giao diện ngang

--- app/src/main/java/com/codetogether/nfcdemo/NFCReaderWrite.kt
+++ app/src/main/java/com/codetogether/nfcdemo/NFCReaderWrite.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -18,6 +19,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -40,51 +42,104 @@ fun NFCReaderWriter(
     var isWriting by remember { mutableStateOf(writeMode) }  // Trạng thái ghi dữ liệu
     val context = LocalContext.current
 
+    // Kiểm tra orientation (đứng hay ngang)
+    val isLandscape =
+        LocalContext.current.resources.configuration.orientation == android.content.res.Configuration.ORIENTATION_LANDSCAPE
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
-            .padding(32.dp)
+            .padding(top = 16.dp, bottom = 16.dp, start = 32.dp, end = 32.dp)
     ) {
         Text(
             text = if (isWriting) "Chế độ ghi" else "Chế độ đọc",
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.weight(1f))
-        Text(
-            text = message,
-            style = MaterialTheme.typography.titleLarge,
-            textAlign = TextAlign.Center
-        )  // Hiển thị message truyền vào
-        Spacer(modifier = Modifier.height(16.dp))
 
-        if (isWriting) {
-            TextField(
-                value = inputText,
-                onValueChange = { inputText = it },
-                label = { Text("Nội dung ghi vào thẻ") },
-                enabled = !writeMode
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(
-                onClick = {
-                    // Ẩn bàn phím khi nhấn nút ghi
-                    val inputMethodManager =
-                        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                    inputMethodManager.hideSoftInputFromWindow(
-                        (context as Activity).currentFocus?.windowToken,
-                        0
+        if (isLandscape && isWriting) {
+            // Khi ở chế độ quay ngang và đang ở chế độ ghi, bố trí 2 cột
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                // Cột bên trái hiển thị message
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = message,
+                        style = MaterialTheme.typography.titleLarge,
+                        textAlign = TextAlign.Center
+                    )  // Hiển thị message truyền vào
+                }
+                VerticalDivider(modifier = Modifier.height(150.dp))
+                // Cột bên phải chứa TextField và Button
+                Column(
+                    modifier = Modifier.weight(1f),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(
+                        value = inputText,
+                        onValueChange = { inputText = it },
+                        label = { Text("Nội dung ghi vào thẻ") },
+                        enabled = !writeMode
                     )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            // Ẩn bàn phím khi nhấn nút ghi
+                            val inputMethodManager =
+                                context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                            inputMethodManager.hideSoftInputFromWindow(
+                                (context as Activity).currentFocus?.windowToken,
+                                0
+                            )
 
-                    onStartWrite(inputText)
-                },
-                enabled = inputText.isNotBlank() && !writeMode
-            ) {
-                Text(text = "Bắt đầu ghi")
+                            onStartWrite(inputText)
+                        },
+                        enabled = inputText.isNotBlank() && !writeMode
+                    ) {
+                        Text(text = "Bắt đầu ghi")
+                    }
+                }
+            }
+        } else {
+            // Khi không ở chế độ quay ngang hoặc đang ở chế độ đọc
+            Text(
+                text = message,
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center
+            )  // Hiển thị message truyền vào
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            if (isWriting) {
+                TextField(
+                    value = inputText,
+                    onValueChange = { inputText = it },
+                    label = { Text("Nội dung ghi vào thẻ") },
+                    enabled = !writeMode
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = {
+                        // Ẩn bàn phím khi nhấn nút ghi
+                        val inputMethodManager =
+                            context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        inputMethodManager.hideSoftInputFromWindow(
+                            (context as Activity).currentFocus?.windowToken,
+                            0
+                        )
+
+                        onStartWrite(inputText)
+                    },
+                    enabled = inputText.isNotBlank() && !writeMode
+                ) {
+                    Text(text = "Bắt đầu ghi")
+                }
             }
         }
 
         Spacer(modifier = Modifier.weight(1f))
+
         Button(
             onClick = {
                 isWriting = !isWriting
